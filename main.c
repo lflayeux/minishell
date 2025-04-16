18a71cb1df877a2fd277f266522b53ab8e3c5561
Lorenzo Flayeux <aherlaud@student.42.fr>
2025-04-16 19:00:19 +0200

    (both) [# : 🚀 FEAT] ----> CTRL-C/CTRL-/ HANDLING : handled with 2 new files and function tester

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lflayeux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 11:19:08 by lflayeux          #+#    #+#             */
-/*   Updated: 2025/04/07 14:53:10 by lflayeux         ###   ########.fr       */
+/*   Updated: 2025/04/16 18:46:36 by lflayeux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,51 @@
 int	main(int argc, char **argv, char **env)
 {
 	char *input;
+	t_signal	signals;
 
 	(void)argc;
 	(void)argv;
 	(void)env;
+	set_signal(&signals);
 	while (1)
 	{
 		input = readline(PROMPT);
 		if (input == NULL || ft_strcmp(input, "exit") == 0)
-			return (0);
+			return (printf("exit"),0);
 		if (*input)
 		{
 			add_history(input);
+			test_signals(signals, env);
 		}
+		reset_signals(&signals);
+	}
+}
+
+// TESTS
+
+void	test_signals(t_signal signals, char **env)
+{
+	pid_t	pid;
+	char *caca[] = {"../fdfdemerde/fdf", "../test_fdf/test_maps/julia.fdf", NULL};
+	int	status;
+//	char *caca[] = {"/bin/bash", NULL};
+
+	pid = fork();
+	if (pid == 0)
+	{
+		child_signals(&signals);
+		execve(caca[0], caca, env);
+		printf("test");
+	}
+	else
+	{
+		parent_signals(&signals);
+		waitpid(pid, &status, 0);
+		if (WIFSIGNALED(status) && WTERMSIG(status) == SIGQUIT)
+		{
+			if (WCOREDUMP(status))
+				printf("Core dump fdp\n");
+		}
+		status = 0;
 	}
 }
